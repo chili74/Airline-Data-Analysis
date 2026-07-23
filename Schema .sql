2fb0a2adef56444f0dbf40d99f33ba14e005f71d
chili74 <onthakgile.chiloane@gmail.com>
2026-07-23 15:03:43 +0200

    adding conditions to schema exploration

--- Schema .sql
+++ Schema .sql
@@ -5,3 +5,31 @@ select * from information_schema.tables
 --Explore all columns in the Database 
 select * from information_schema.columns 
 
+select * from information_schema.columns
+where table_name = 'airports'
+
+select * from information_schema.columns
+where table_name = 'aircrafts'
+
+select * from information_schema.columns
+where table_name = 'routes'
+
+select * from information_schema.columns
+where table_name = 'bookings'
+
+select * from information_schema.columns
+where table_name = 'employees'
+
+select * from information_schema.columns
+where table_name = 'customers'
+
+select * from information_schema.columns
+where table_name = 'flights'
+
+select * from information_schema.columns
+where table_name = 'tickets'
+
+select * from information_schema.columns
+where table_name = 'maintenance'
+
+
